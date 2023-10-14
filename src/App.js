8d8c4dc6cfb3d402a70200a6c35e99e53e933018
Archaryy <wilsonanthony2013@gmail.com>
2023-10-14 15:14:14 +0700

    Revisi FormSubmit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './Form';
+import FormSubmitMessage from './FormSubmitMessage';
 
 function App() {
   const [formData, setFormData] = useState(null);
@@ -13,13 +14,7 @@ function App() {
     <div className="App">
       <h1>Tugas GSLC Form</h1>
       <Form onFormSubmit={handleFormSubmit} />
-      {formData && (
-        <div className="success-message">
-          <h2>Berhasil masukkan data!</h2>
-          <p>Nama: {formData.name}</p>
-          <p>Email: {formData.email}</p>
-        </div>
-      )}
+      {formData && <FormSubmitMessage formData={formData} />}
     </div>
   );
 }
--- src/FormSubmitMessage.js
+++ src/FormSubmitMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FormSubmitMessage = ({ formData }) => {
   return (
     <div className="success-message">
-      <h2>Form submitted successfully!</h2>
+      <h2>Berhasil submit Form!</h2>
       <p>Nama: {formData.name}</p>
       <p>Email: {formData.email}</p>
     </div>
